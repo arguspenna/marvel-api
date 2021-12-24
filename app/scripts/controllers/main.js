469127a7a74d696c00f9b818bd77903f91fe0e25
Argus <argusfpenna@gmail.com>
2021-12-24 10:43:32 -0300

    Criação do Procfile

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,24 @@
-'use strict';
+(function () {
+  'use strict';
 
-/**
- * @ngdoc function
- * @name desafioApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the desafioApp
- */
-angular.module('desafioApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  angular
+    .module('desafioApp')
+    .controller('MainController', MainController);
+
+    function MainController($http){
+      var vm = this;
+
+      vm.onInit = onInit;
+
+      function onInit(){
+        $http({
+          url: "https://developer.marvel.com/v1/public/characters", 
+          method: "GET",
+          params: {apikey: "38b1589a4602db99bea14aebbc96fb99"}
+        }).success(function(data){
+          console.log(data);
+          //  vm.herosList = data
+        });
+      }
+    }
+})();
